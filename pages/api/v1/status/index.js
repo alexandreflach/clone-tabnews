65b5a16b485e53fb91d0a1a21f5100e79b49d149
Alexandre Flach <flachalexandret@gmail.com>
2024-03-22 13:54:35 +0000

    #5 add database info on status api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result);
-  response.status(200).json({ key: "up the average" });
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenConnectionsResult = await database.query(
+    "select count(*)::int as opened_connections from pg_stat_activity where datname = 'local_db'",
+  );
+  const databaseOpenConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].opened_connections;
+
+  const updateAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
