a9058df748145fdc1d5835a516922422d5247073
Alexandre Flach <flachalexandret@gmail.com>
2025-03-03 12:49:33 +0000

    feat: controller pattern

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./error";
 
 async function query(queryObject) {
   let client;
@@ -8,8 +9,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      cause: error,
+      message: "Connection with database or query error.",
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,25 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("POST /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status", {
+        method: "POST",
+      });
+      expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Method not allowed for this endpoint.",
+        action: "Check if method HTTP sent is valid for this endpoint.",
+        statusCode: 405,
+      });
+    });
+  });
+});
