ab4f62433d5e30aa3b7b1a5dd6368000fa0e352a
Alexandre Flach <flachalexandret@gmail.com>
2025-03-17 12:57:19 +0000

    feat: add migration to create `users` table

---
+++ infra/migrations/1742214694380_create-users.js
@@ -0,0 +1,38 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    //for reference, GitHub limits usernames to 39 characters
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+    //why 254? https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    //why 72 set length? https://security.stackexchange.com/a/39851
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+      unique: true,
+    },
+    //why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+    update_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+  });
+};
+
+exports.down = (pgm) => {};
